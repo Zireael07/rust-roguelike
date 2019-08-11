ae3323c99948029717627dada66646c6642a73a0
Zireael07 <zi@mail.com>
2019-08-11 12:02:21 +0200

    basic rust wasm page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//import("../pkg/index.js").catch(console.error);
+
+
+// A dependency graph that contains any wasm must all be imported
+// asynchronously. This file does the single async import, so
+// that no one else needs to worry about it again.
+import("./main.js")
+  .catch(e => console.error("Error importing `main.js`:", e));
